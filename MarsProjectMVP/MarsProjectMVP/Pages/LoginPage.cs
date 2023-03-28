6fbdf3b170af5020d0d3484dda31cbb9b36a5c4f
Meet Shah <shahmeetnz@gmail.com>
2023-03-28 22:07:02 +1300

    Mars Login Functinality

---
+++ MarsProjectMVP/MarsProjectMVP/Pages/LoginPage.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//using MarsProjectMVP.Utilities;
+//using OpenQA.Selenium;
+//using OpenQA.Selenium.Chrome;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MarsProjectMVP.Pages
+//{
+//    public class LoginPage: CommonDriver
+//    {
+//        public void LoginActions(IWebDriver driver)
+//        {
+//            driver.Manage().Window.Maximize();
+
+//            driver.Navigate().GoToUrl("http://localhost:5000/");
+//            Thread.Sleep(2000);
+
+
+
+
+
+
+//        }
+
+//    }
+//}
+
+//Login Functionality
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+driver.Navigate().GoToUrl("http://localhost:5000/");
+Thread.Sleep(1000);
+
+IWebElement signin = driver.FindElement(By.XPath("//*[@id=\"home\"]/div/div/div[1]/div/a"));
+signin.Click();
+
+IWebElement emailID = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[1]/input"));
+emailID.SendKeys("shahmeetnz@gmail.com");
+
+IWebElement password = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[2]/input"));
+password.SendKeys("123456");
+
+IWebElement loginbutton = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[1]/div/div[4]/button"));
+loginbutton.Click();    
+
+
+//driver.Quit(); 
